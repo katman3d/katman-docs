bd19f107d6b159c72c1225d66fa9011a89a33eda
BoraMD <bora2002md@gmail.com>
2021-04-01 19:14:17 +0300

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 #
 html_theme = 'sphinx_rtd_theme'
 html_logo = 'html_logokalite.png'
-html_favicon = 'siteikon'
+html_favicon = 'siteikon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
